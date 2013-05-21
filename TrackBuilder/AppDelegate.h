24621e7f7896ab3f4d66730229b6c7c8fc659198
gazz <janis@cobook.co>
2013-05-21 19:35:08 +0300

    Initial Commit

---
+++ TrackBuilder/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TrackBuilder
+//
+//  Created by Janis Dancis on 5/21/13.
+//  Copyright (c) 2013 digihaze. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ TrackBuilder/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  TrackBuilder
+//
+//  Created by Janis Dancis on 5/21/13.
+//  Copyright (c) 2013 digihaze. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+  return NSApplicationMain(argc, (const char **)argv);
+}
